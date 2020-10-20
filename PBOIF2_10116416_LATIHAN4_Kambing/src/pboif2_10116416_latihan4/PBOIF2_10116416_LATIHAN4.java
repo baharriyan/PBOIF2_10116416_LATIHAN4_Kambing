d9f6b3e2269c48dc9ea6b1fbb03219a9859a95bf
baharriyan <indonesiariyan@gmail.com>
2020-10-20 18:52:38 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN4_Kambing/src/pboif2_10116416_latihan4/PBOIF2_10116416_LATIHAN4.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116416_latihan4;
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF2_10116416_LATIHAN4 {
+ public void tambahKambing(){
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+    } 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PBOIF2_10116416_LATIHAN4 kambingJantan = new PBOIF2_10116416_LATIHAN4();
+        kambingJantan.tambahKambing();
+    }
+    
+}
